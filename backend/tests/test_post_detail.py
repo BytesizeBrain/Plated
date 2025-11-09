8f6f6e53a2985558d88cb89faff194da6c290e9c
Chau-Nguyen-Developer <chau.nguyen.dev@gmail.com>
2025-11-09 12:36:04 -0800

    Retrieve one specific post by GET /posts/<post_id>
    Given a post_id, able to retrieve (fetch) that post from Supabase.
    The route returns its image, caption, author name and profil pic, and time stamp

---
+++ backend/tests/test_post_detail.py
@@ -0,0 +1,9 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:5000/posts/"
+post_id = "927a8655-b371-4a34-8823-4be22807cad2"
+
+res = requests.get(BASE_URL + post_id)
+print("GET status: ", res.status_code)
+print("Retrieve post successfully based on post ID.")
+print("GET response: ", res.json())
